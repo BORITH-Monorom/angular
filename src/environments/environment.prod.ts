69bd476539b8b491c5eea477bfbe4b75d8f68a01
BORITH-Monorom <borithmonorom@gmail.com>
2025-05-07 02:46:40 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://back-end-liard-psi.vercel.app',
-  // apiUrl: 'http://localhost:4000', 
+  apiUrl_maskmail_report: 'https://maskmail.itlink.com.kh/api/v1'
+
+  // apiUrl: 'http://localhost:4000',
 };
